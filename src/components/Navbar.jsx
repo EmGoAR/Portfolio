5eef7f131dd8a08112a1be20bf754453c165cd61
Emiliano Gonzalez <93420074+EmGoAR@users.noreply.github.com>
2022-12-25 19:23:56 -0300

    Half of the project!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,68 +1,60 @@
-import React from 'react'
-import { useState } from 'react'
-import { close, logo, menu } from '../assets'
-import { navLinks } from '../constants'
+import { useState } from "react";
+
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
-  {/* Creamos un toggle para cambiar a la navbar de celular, comienza en false */ }
-  const [toggle, setToggle] = useState(false)
+  const [active, setActive] = useState("Home");
+  const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt="Hoobank" className='w-[124px] h-[32px]' />
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
-      {/* Mobile app bar */}
-      <ul className='list-none sm:flex justify-end items-center flex-1'>
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppin font-normal cursor-pointer text-[16px] 
-            ${index === navLinks.lenght - 1 ? 'mr-0' : 'mr-10'} sm:text-white mr-10`}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              active === nav.title ? "text-white" : "text-dimWhite"
+            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            onClick={() => setActive(nav.title)}
           >
-            {/* cuando clickeas este link, este pone un # y toma el valor del id ya guardado en navLinks, 
-              por lo tanto quedaria asi #home #features #products #clients */}
-            <a href={`#${nav.id}`}>
-              {nav.title}
-            </a>
+            <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
         ))}
       </ul>
 
-      {/* Desktop app bar */}
-      <div className='sm:hidden flex flex-1 justify-end items-center'>
-        {/* si el toggle esta activado, mostrar icono de close, sino, mostrar icono de menu*/}
-        <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer'
-          onClick={() => setToggle((prev) => !prev)} />
-        {/*
-              en react no se debería hacer esto: onClick={() => setToggle(!toggle)}
-              no deberíamos actualizar un estado usando el estado previo de ese estado
-              para eso lo hacemos con una funcion callback:
-              onClick={() => setToggle((prev) => !prev)} (la forma correcta de hacerlo)
-        */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(!toggle)}
+        />
 
-        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient
-        absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar` }>
-
-          <ul className='list-none flex flex-col '>
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
-                className={`font-poppin font-normal cursor-pointer text-[16px] 
-            ${index === navLinks.lenght - 1 ? 'mr-0' : 'mb-4'} text-white mr-10`}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
               >
-                {/* cuando clickeas este link, este pone un # y toma el valor del id ya guardado en navLinks, 
-              por lo tanto quedaria asi #home #features #products #clients */}
-                <a href={`#${nav.id}`}>
-                  {nav.title}
-                </a>
+                <a href={`#${nav.id}`}>{nav.title}</a>
               </li>
             ))}
           </ul>
-
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
